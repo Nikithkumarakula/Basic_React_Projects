e20e0533e5c0d6639ab49ce43901750ca7361ef3
Nikithkumarakula <anikithkumar2003@gmail.com>
2024-12-26 16:42:15 +0530

    implemented counter

---
+++ learnstate/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Counter from "./Counter";
+export default function App() {
+  // const [name, setName] = useState('Neha')
+
+  // const handleclick = () => {
+  //   setName('Akash');
+  // }
+  return (
+    <div>
+      {/*<h1>Hello {name} </h1>
+       <button onClick={handleclick}>Click</button> */} 
+
+      <Counter />
+    </div>
+  )
+}
