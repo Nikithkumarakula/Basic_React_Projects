7b42bffba3ee1424b92609765534732b5b435b0d
Nikithkumarakula <anikithkumar2003@gmail.com>
2024-12-26 13:43:37 +0530

    learn basics

--- basics/src/App.js
+++ basics/src/App.js
@@ -1,9 +1,19 @@
-
+import About from './About'
+import Contact from './Contact'
+import Greeting from './Greeting';
 
 function App() {
+
+  let name = 'Pooja';
+  
   return (
-    <h1> hello</h1>
+    <div>
+      <h1> Hello from ReactJS</h1>
+      <Greeting name = {'Akash'} />
+      <Greeting name = {'Ayush'} />
+       <About name = {name} />
+      <Contact />
+    </div>
   );
 }
-
 export default App;
